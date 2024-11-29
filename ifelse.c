4e11971401c3e73351b3d7cb8f9eddf3ad74b1b6
SRIPATHI M <msripathi243@gmail.com>
2024-11-29 15:26:22 +0530

    Add files via upload

---
+++ ifelse.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main()
+{
+
+	int n1=2,n2=1,n3=4;
+
+	if(n1>n2)
+	{
+		printf("the greatest of three number %d\n",n1);
+	}
+           if(n1>n3)
+	{
+		printf("THe greatest num is %d",n3);
+	}
+
+          if(n2>n3)
+	  {
+
+		printf("The greatest num is $%d",n3);
+
+	  }
+
+
+return 0;
+
+}
