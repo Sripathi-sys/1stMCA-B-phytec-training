47db7399335cc74220f13eb666f95e0b483489ea
SRIPATHI M <msripathi243@gmail.com>
2025-02-11 20:13:02 +0530

    Add files via upload

---
+++ arrpointer1.c
@@ -0,0 +1,22 @@
+//print the element of the arry using pointer
+
+
+#include <stdio.h>
+int main(){
+
+	int arr[2][2]={1,2},{3,4};
+
+	int *q;
+	//declare q pointer
+
+	q=&arr[0]
+
+	ptr=&q;
+
+
+
+	printf("%d",*(*arr+1)+0);
+
+	return 0;
+
+}
