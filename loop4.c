5edc5d67af91519b42239cf8253d0e22597da7df
SRIPATHI M <msripathi243@gmail.com>
2025-02-14 22:13:06 +0530

    Add files via upload

---
+++ loop4.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(){
+
+	int n,i;
+
+
+	printf("Enter the number:");
+	scanf("%d",&n);
+
+	for(int i=1;i<=n;i++){
+		printf("Multiplication table:%d %d %d\n",n,i,n*i);
+		
+	}
+
+return 0;
+
+}
+
+
+
